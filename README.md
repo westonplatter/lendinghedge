6dbb1692a091ab2efc7e5bab2e2b5849f90b4b96
Weston Platter <westonplatter@gmail.com>
2015-11-26 13:34:42 -0700

    lh fast first commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+LhFast
+======
+
+## development
+
+## testing
+
+## production
+
+## License
+Copyright (c) 2015, Weston Platter. All rights reserved.
---
+++ config/application.rb
@@ -0,0 +1,18 @@
+require File.expand_path('../boot', __FILE__)
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+
+Bundler.require(*Rails.groups)
+
+module LhFast
+  class Application < Rails::Application
+    config.time_zone = 'UTC'
+  end
+end
