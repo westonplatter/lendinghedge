ed523a89043ca2f90e76db6a7ed2e862aa7e1f6b
Weston Platter <westonplatter@gmail.com>
2015-12-22 15:30:42 -0700

    cap. set log level to info.

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :deploy_to, '/var/www/lh_fast'
 # set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
+set :log_level, :info
 
 # Default value for :pty is false
 # set :pty, true
