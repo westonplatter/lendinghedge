bcc5f098433fd25cadd76025f4f685058a48abf1
Weston Platter <westonplatter@gmail.com>
2016-01-16 18:15:16 -0700

    views. provide a different layout for devise pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  layout 'public'
+  layout 'home'
 
   def index
   end
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,14 @@ module LhFast
 
     # auto load the lib directory
     config.autoload_paths << Rails.root.join('lib')
+
+    config.to_prepare do
+      Devise::SessionsController.layout "devise"
+      Devise::RegistrationsController.layout proc { |controller| user_signed_in? ? "application" : "devise" }
+      Devise::ConfirmationsController.layout "devise"
+      Devise::UnlocksController.layout "devise"
+      Devise::PasswordsController.layout "devise"
+    end
+
   end
 end
