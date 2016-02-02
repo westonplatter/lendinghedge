1ef1584dfbcace840d183a1b57ad1396f50a096d
Weston Platter <westonplatter@gmail.com>
2016-02-02 09:23:33 -0700

    up the limit

--- app/workers/schedule_fico_mean_fetch.rb
+++ app/workers/schedule_fico_mean_fetch.rb
@@ -2,8 +2,8 @@ class ScheduleFicoMeanFetch
   include Sidekiq::Worker
 
   def perform
-    # run 100 loans every 30 minutes
-    num = 50
+    # run 150 loans every 30 minutes
+    num = 75
     Loan.
       where("fico_mean is null").
       where("loan_id is not null").
