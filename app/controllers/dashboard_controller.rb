a9cfa2fe4217c77ed0759ac3ab04dcfb76c26058
Weston Platter <westonplatter@gmail.com>
2016-02-25 23:53:10 -0700

    dashboard. show summary.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,12 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    options = {
+      access_token: current_user.lending_club_access_token,
+      investor_id: current_user.lending_club_investor_id,
+    }
+    client = LendingClub::Client.new(options)
+
+    @accounts = [client.summary]
   end
 end
