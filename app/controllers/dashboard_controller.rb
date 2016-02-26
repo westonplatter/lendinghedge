8dc64ee3d39e19c6736e73fc169a3316029c240f
Weston Platter <westonplatter@gmail.com>
2016-02-26 08:41:17 -0700

    controllers. fix the tests. gp

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,12 +2,16 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    options = {
-      access_token: current_user.lending_club_access_token,
-      investor_id: current_user.lending_club_investor_id,
-    }
-    client = LendingClub::Client.new(options)
+    @accounts = []
 
-    @accounts = [client.summary]
+    unless current_user.lending_club_investor_id.nil?
+      options = {
+        access_token: current_user.lending_club_access_token,
+        investor_id: current_user.lending_club_investor_id,
+      }
+      client = LendingClub::Client.new(options)
+
+      accounts << client.summary
+    end
   end
 end
